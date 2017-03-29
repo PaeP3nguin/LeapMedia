03c3fa5f2c558f83acbbfe9f5efa80d55fe6096e
paep3nguin <paep3nguin@gmail.com>
2017-03-28 23:32:44 -0500

    Minimize to taskbar and correct prev/next track gestures

--- LeapMagic/MainWindow.xaml.cs
+++ LeapMagic/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Windows;
 using System.Runtime.InteropServices;
+using System.Windows.Input;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace LeapMagic {
     /// <summary>
@@ -10,9 +12,11 @@ namespace LeapMagic {
     public partial class MainWindow : Window {
 
         private const float MIN_PINCH_DIST = 60;
-        private const float MAX_PINCH_STRENGTH = 0f;
-        private const float MAX_GRAB_STRENGTH = 0f;
+        private const float MAX_PINCH_STRENGTH = 0;
+        private const float MAX_GRAB_STRENGTH = 0;
         private const int MIN_ACTION_DEBOUNCE = 1000 * 1000;
+
+        private TaskbarIcon icon;
 
         private Controller controller = new Controller();
         private int currentHand;
@@ -21,7 +25,25 @@ namespace LeapMagic {
         public MainWindow() {
             InitializeComponent();
 
+            icon = (TaskbarIcon) FindResource("taskbarIcon");
+            icon.TrayLeftMouseUp += Icon_TrayLeftMouseUp;
+
+            Closing += delegate { icon.Dispose(); };
+
             controller.FrameReady += frameHandler;
+        }
+
+        private void Icon_TrayLeftMouseUp(object sender, RoutedEventArgs e) {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+        }
+
+        protected override void OnStateChanged(EventArgs e) {
+            if (WindowState == WindowState.Minimized) {
+                this.Hide();
+            }
+
+            base.OnStateChanged(e);
         }
 
         public Hand CurrentHand { get; set; }
@@ -32,6 +54,8 @@ namespace LeapMagic {
             if (frame.Hands.Count != 1) return;
 
             Hand hand = frame.Hands[0];
+            // Only using right hand
+            if (hand.IsLeft) return;
             CurrentHand = hand;
 
             handInfo.Text = hand.ToString();
@@ -40,11 +64,45 @@ namespace LeapMagic {
             grabStrength.Text = "Grab strength: " + hand.GrabStrength;
             confidence.Text = "Confidence: " + hand.Confidence;
             timeVisible.Text = "Time visible: " + hand.TimeVisible;
-            palmPosition.Text = "Palm position: " + hand.PalmPosition.ToString();
+            // TODO: Use position to extablish bounding box
+            palmPosition.Text = $"Palm position: {hand.PalmPosition}";
+            handYaw.Text = $"Hand yaw: {hand.Direction.Yaw}";
+            handPitch.Text = $"Hand pitch: {hand.Direction.Pitch}";
+            handRoll.Text = $"Hand roll: {hand.PalmNormal.Roll}";
+
+            float angleSum = 0f;
+            Leap.Vector lastAngle = hand.Fingers[0].Direction;
+            foreach (Finger finger in hand.Fingers) {
+                angleSum += finger.Direction.AngleTo(lastAngle);
+                lastAngle = finger.Direction;
+            }
+
+            fingerSpread.Text = "Finger spread: " + angleSum;
+            bool isHandOpen = angleSum >= 1;
+            handOpen.Text = $"Hand open: {isHandOpen}";
+            currentTime.Text = $"Current time: {frame.Timestamp}";
+            nextActionTime.Text = $"Next action time: {lastActionTime + MIN_ACTION_DEBOUNCE}";
+            sameHand.Text = $"Same hand: {hand.Id == currentHand}";
 
             if (hand.Id == currentHand) {
-
+                // Only accept continuing gestures with closed hand
+                if (isHandOpen) return;
+                if (hand.Direction.Yaw >= 0.6) {
+                    if (frame.Timestamp <= lastActionTime + MIN_ACTION_DEBOUNCE) return;
+                    MediaController.previousTrack();
+                    lastActionTime = frame.Timestamp;
+                    lastActionText.Text = "prev";
+                }
+                if (hand.Direction.Yaw <= -0.4) {
+                    if (frame.Timestamp <= lastActionTime + MIN_ACTION_DEBOUNCE) return;
+                    MediaController.nextTrack();
+                    lastActionTime = frame.Timestamp;
+                    lastActionText.Text = "next";
+                }
             } else {
+                currentHand = hand.Id;
+                // Ignore one-time gestures with closed hand
+                if (!isHandOpen) return;
                 // Ignore hands that have not been visible for long
                 // if (hand.TimeVisible <= 500 * 1000) return;
                 // Ignore the hand if moving a mouse
@@ -55,9 +113,10 @@ namespace LeapMagic {
                 if (Math.Abs(hand.PalmPosition.x) >= 100 || Math.Abs(hand.PalmPosition.z) >= 100) return;
                 // Only toggle music at most once every second
                 if (frame.Timestamp <= lastActionTime + MIN_ACTION_DEBOUNCE) return;
+
                 MediaController.toggleMusic();
                 lastActionTime = frame.Timestamp;
-                currentHand = hand.Id;
+                lastActionText.Text = "pause";
             }
         }
     }
