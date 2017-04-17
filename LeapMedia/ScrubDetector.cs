c5d10aebe1e2eaa667f118a3bb3acc6a1dd71c2c
paep3nguin <paep3nguin@gmail.com>
2017-04-17 01:20:05 -0500

    Don't scrub for hands that aren't visible long

--- LeapMedia/ScrubDetector.cs
+++ LeapMedia/ScrubDetector.cs
@@ -3,10 +3,13 @@
 namespace LeapMedia {
     internal class ScrubDetector {
         public const float MIN_TRIGGER_DISTANCE = 30;
+        public const float MIN_TIME_VISIBLE = 750 * 1000;
 
         public float lastXTriggered = float.NaN;
 
         public void OnHand(HandStats hand, long timestamp) {
+            if (hand.TimeVisible <= MIN_TIME_VISIBLE) return;
+
             float currentX = hand.PalmPosition.x;
             if (lastXTriggered == float.NaN) {
                 lastXTriggered = currentX;
